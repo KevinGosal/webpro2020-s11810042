e8718f4eb970ea370d1fa2844905d4d0fabf719f
KevinGosal <s11810042@student.unklab.ac.id>
2020-10-22 14:04:06 +0800

    11th commit

---
+++ Pertemuan_19/index.js
@@ -0,0 +1,67 @@
+//JavaScript Class and OOP
+
+// Class and Constructor
+
+//const Person = {
+ //   firstname "Kevin",
+//    lastname "Gosal,",
+//    displayFullName: function (){
+//        console.log(`${this.firstname} ${this.lastname}`);
+//    }
+// };
+
+//Person.displayFullName();
+
+// blueprint
+//class Person {
+//    constructor(firstname, lastname){
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+
+//    }
+// }
+
+// displayFullName(){
+//    console.log(`${this.firstname} ${this.lastname}`);
+
+// }
+
+// const person1 = new Person("Kevin", "Gosal");
+// console.log(person1);
+// person1.displayFullName();
+// const person2 = new Person("John", "Doe");
+// person2.displayFullName();
+
+
+// Inheritance
+
+// class Student extends Person{
+//    constructor(firstname, lastname, NIM){
+//        super(firstname, lastname);
+//        this.NIM = NIM;
+//        this.age = age;
+//    }
+// Static member    
+//    displayNIM(){
+//        console.log(`Your NIM is ${this.NIM}`);
+//    }
+//    static sayHello(){
+//        console.log(`Hello gaes`);
+
+//    }
+// Setter and Getter    
+//    set studentScore(score){
+//        this.score = score;
+//    }
+//    get studentScore(){
+//        return this.score;
+//    }
+// }
+
+// const john = new Student("John", "Doe", "123456789010", 20);
+// john.displayFullName();
+// john.displayNIM();
+// Student.sayHello();
+
+// john.studentScore = 100;
+// console.log(john.studentScore);
